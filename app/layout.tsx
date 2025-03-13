8264779eb47f33c1f50b004d8835ca22eb391d7d
AntLacerda <lacerdaneto99@hotmail.com>
2025-03-12 22:16:10 -0300

    Feat: finalizando o projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 
 const montserrat = Montserrat({
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="bg-[#292929] h-screen w-screen flex flex-col justify-center items-center gap-4">
         <Image src={appLogo} alt="App Logo" className="w-96" />
         <h2 className="text-white text-5xl font-bold text-center">A maneira mais <br/>simples de reportar <br/> um crime!</h2>
-        <div className="flex  flex-col justify-center items-center w-full">
+        <div className="flex flex-col justify-center items-center w-full">
           <Link href="/login" className="bg-[#3BC9DB] text-white text-2xl font-bold w-1/3 h-12 flex justify-center items-center rounded-md">Login</Link>
           <Link href="/sign-in" className=" text-[#3BC9DB] text-sm  w-fit h-12 flex justify-center items-center rounded-md underline">Criar sua conta</Link>
         </div>
